0be26502331be5bf56f3b657cd1d255f4d64bd29
x417v <ilyakinskiy@gmail.com>
2023-10-23 18:51:34 +0300

    .

---
+++ frontend/src/main.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.tsx';
+import './index.css';
+import HomePage from './pages/HomePage.tsx';
+import ProductPage from './pages/ProductPage.tsx';
+import { HelmetProvider } from 'react-helmet-async';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'product/:slug', element: <ProductPage /> },
+    ],
+  },
+]);
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </HelmetProvider>
+  </React.StrictMode>,
+);
---
+++ frontend/src/pages/ProductPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+const ProductPage = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Product Page</title>
+      </Helmet>
+    </div>
+  );
+};
+
+export default ProductPage;
