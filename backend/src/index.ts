a0694a13b2330fc59849daf2df93b8581a7baf60
x417v <ilyakinskiy@gmail.com>
2023-10-24 19:34:26 +0300

    connect mongoDB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,23 @@
-import express, { Request, Response } from 'express';
-import { sampleProducts } from './data';
+import express from 'express';
+import dotenv from 'dotenv';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import { productRouter } from './routers/productRouter';
+import { seedRouter } from './routers/seedRouter';
+
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mogodb://localhost/mernamazona';
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('Connected to mongodb');
+  })
+  .catch(() => {
+    console.log('Error mongodb');
+  });
 
 const app = express();
 app.use(
@@ -10,13 +27,8 @@ app.use(
   }),
 );
 
-app.get('/api/products', (req: Request, res: Response) => {
-  res.json(sampleProducts);
-});
-
-app.get('/api/products/:slug', (req: Request, res: Response) => {
-  res.json(sampleProducts.find((x) => x.slug === req.params.slug));
-});
+app.use('/api/products', productRouter);
+app.use('/api/seed', seedRouter);
 
 const PORT = 4000;
 
