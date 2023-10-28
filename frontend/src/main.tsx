86caa679cbd07fc94d57289a43f5d84c10c8d6d7
x417v <ilyakinskiy@gmail.com>
2023-10-28 17:11:07 +0300

    Implement Register User

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,6 +12,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StoreProvider } from './Store.tsx';
 import CartPage from './pages/CartPage.tsx';
 import SigninPage from './pages/SigninPage.tsx';
+import SignupPage from './pages/SignupPage.tsx';
 
 const router = createBrowserRouter([
   {
@@ -22,6 +23,7 @@ const router = createBrowserRouter([
       { path: 'product/:slug', element: <ProductPage /> },
       { path: 'cart', element: <CartPage /> },
       { path: 'signin', element: <SigninPage /> },
+      { path: 'signup', element: <SignupPage /> },
     ],
   },
 ]);
