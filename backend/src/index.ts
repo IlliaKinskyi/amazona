2e1011dfc0190613358942e4d4010f870b4adef6
x417v <ilyakinskiy@gmail.com>
2023-10-29 18:06:59 +0200

    Build Order Create API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import { productRouter } from './routers/productRouter';
 import { seedRouter } from './routers/seedRouter';
 import { userRounter } from './routers/userRouter';
+import { orderRouter } from './routers/orderRouter';
 
 dotenv.config();
 
@@ -34,6 +35,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/products', productRouter);
 app.use('/api/users', userRounter);
 app.use('/api/seed', seedRouter);
+app.use('/api/orders', orderRouter);
 
 const PORT = 4000;
 
