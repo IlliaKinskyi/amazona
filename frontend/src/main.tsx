8215c917a5a1ce6137e23066885cab3f7149dbd6
x417v <ilyakinskiy@gmail.com>
2023-10-29 23:34:32 +0200

    Create Place Order Page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.tsx';
 import './index.css';
@@ -15,22 +20,25 @@ import SigninPage from './pages/SigninPage.tsx';
 import SignupPage from './pages/SignupPage.tsx';
 import ShippingAddressPage from './pages/ShippingAddressPage.tsx';
 import PaymentMethodPage from './pages/PaymentMethodPage.tsx';
+import ProtectedRoute from './components/ProtectedRoute.tsx';
+import PlaceOrderPage from './pages/PlaceOrderPage.tsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      { index: true, element: <HomePage /> },
-      { path: 'product/:slug', element: <ProductPage /> },
-      { path: 'cart', element: <CartPage /> },
-      { path: 'signin', element: <SigninPage /> },
-      { path: 'signup', element: <SignupPage /> },
-      { path: 'shipping', element: <ShippingAddressPage /> },
-      { path: 'payment', element: <PaymentMethodPage /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} element={<HomePage />} />
+      <Route path="product/:slug" element={<ProductPage />} />
+      <Route path="cart" element={<CartPage />} />
+      <Route path="signin" element={<SigninPage />} />
+      <Route path="signup" element={<SignupPage />} />
+      <Route path="" element={<ProtectedRoute />}>
+        <Route path="shipping" element={<ShippingAddressPage />} />
+        <Route path="payment" element={<PaymentMethodPage />} />
+        <Route path="placeorder" element={<PlaceOrderPage />} />
+      </Route>
+    </Route>,
+  ),
+);
 
 const queryClient = new QueryClient();
 
